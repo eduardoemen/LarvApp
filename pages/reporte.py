c7fa045b23bbe668610c085129d36664ef8c2645
Eduardo Emén <75172890+eduardoemen@users.noreply.github.com>
2024-05-11 01:26:29 +0200

    Update reporte.py

--- pages/reporte.py
+++ pages/reporte.py
@@ -1,13 +1,22 @@
-from reportlab.pdfgen import canvas
-img_file = '1.jpg'
-pdf_file = 'reporte.pdf'
- 
-can = canvas.Canvas(pdf_file)
-can.drawString(20, 400,"este es el reporte del trabajo realizado" )
- 
-x_start = 0
-y_start = 0
-can.drawImage(img_file, x_start, y_start, width=120, preserveAspectRatio=True, mask='auto')
- 
-can.showPage()
-can.save()
+import streamlit as st
+from fpdf import FPDF
+import base64
+
+report_text = st.text_input("Report Text")
+
+
+export_as_pdf = st.button("Export Report")
+
+def create_download_link(val, filename):
+    b64 = base64.b64encode(val)  # val looks like b'...'
+    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
+
+if export_as_pdf:
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font('Arial', 'B', 16)
+    pdf.cell(40, 10, report_text)
+    
+    html = create_download_link(pdf.output(dest="S").encode("latin-1"), "test")
+
+    st.markdown(html, unsafe_allow_html=True)
