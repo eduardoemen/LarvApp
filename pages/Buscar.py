2a144fb52a610ba526aad0858871cc9707fe9030
Eduardo Emén <75172890+eduardoemen@users.noreply.github.com>
2024-05-20 13:44:54 +0200

    Update Buscar.py

--- pages/Buscar.py
+++ pages/Buscar.py
@@ -1,17 +1,7 @@
 import streamlit as st 
 import pandas as pd
 import mysql.connector
-
-
-conn = mysql.connector.connect( host="localhost",
-                                    port="3306",
-                                    user="root",
-                                    passwd="0912853207",
-                                    db="larvapp"
-                                  )
-
-
-
+conn = st.connection('mysql', type='sql')
 cursor = conn.cursor()
 cursor.execute("SELECT * FROM larva")
 data = cursor.fetchall()
