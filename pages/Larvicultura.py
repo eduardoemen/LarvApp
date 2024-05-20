29f0417aae5fc5b6c5ff1df579cafcf8dca774ac
Eduardo Emén <75172890+eduardoemen@users.noreply.github.com>
2024-05-20 13:45:50 +0200

    Update Larvicultura.py

--- pages/Larvicultura.py
+++ pages/Larvicultura.py
@@ -1,14 +1,7 @@
 import streamlit as st 
 import pandas as pd
 import mysql.connector
-
-
-conn = mysql.connector.connect( host="localhost",
-                                    port="3306",
-                                    user="root",
-                                    passwd="0912853207",
-                                    db="larvapp"
-                                  )
+conn = st.connection('mysql', type='sql')
 cursor = conn.cursor()
 
 bl = st.sidebar.selectbox(
