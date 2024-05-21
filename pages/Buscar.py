b5fae2196e6ac65de7afa5ea1a207df612c270b3
Eduardo Emén <75172890+eduardoemen@users.noreply.github.com>
2024-05-21 15:51:23 +0200

    Delete pages directory

--- pages/Buscar.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st 
-import pandas as pd
-import mysql.connector
-conn = st.connection('mysql', type='sql')
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM larva")
-data = cursor.fetchall()
-df = pd.DataFrame(data, columns=["id","fecha","hora","tanque","dia","estadio","actividad natatoria"])
-st.dataframe(df) 
