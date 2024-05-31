87a342eb6841eb2ac92c68ec0e485d5440af1516
Eduardo Emén <75172890+eduardoemen@users.noreply.github.com>
2024-05-31 18:39:19 +0200

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -4,19 +4,38 @@ import datetime
 import pandas as pd
 from streamlit_navigation_bar import st_navbar
 
+import streamlit_authenticator as stauth
 
-st.sidebar.title ("LarvApp")
-st.sidebar.header("App de Gestión para Larvicultura")
-st.sidebar.subheader("Laboratorio de Larvas San Pedro")
+from yaml.loader import SafeLoader
+with open('config.yaml') as file:
+        config = yaml.load(file, Loader=SafeLoader)
+        authenticator = stauth.Authenticate(config['credentials'],config['cookie']['name'],config['cookie']['key'],config['cookie']['expiry_days'])
+        authenticator.login()
+if st.session_state["authentication_status"]:
+        #st.write(f'Bienvenido *{st.session_state["name"]}*') 
+        page = ["Algas", "Artemia", "Larvas", "Maduración", "Microbiología", "Buscar","Estadísticas","Salir"]
+        
+        styles = {
+        "nav": {
+        "background-color": "#7BD192",
+        },
+        "div": {
+        "max-width": "1050px",
+        },
+        "span": {
+        "border-radius": "0.5rem",
+        "padding": "0.4375rem 0.625rem",
+        "margin": "0 0.125rem",
+        },
+        "active": {
+        "background-color": "rgba(255, 255, 255, 0.25)",
+        },
+        "hover": {
+        "background-color": "rgba(255, 255, 255, 0.35)",
+        },
+        }
 
-st.sidebar.write("Inicia Sesión")
-
-username = st.sidebar.text_input("User Name")
-password = st.sidebar.text_input("Password",type='password')
-if st.sidebar.checkbox("OK"):
-    if password == '12345'and username=="edu":
-        page = st_navbar(["Algas", "Artemia", "Larvas", "Maduración", "Microbiología", "Buscar","Estadísticas"])
-        st.sidebar.success("Logged In as {}".format(username))
+        page = st_navbar(page, styles=styles)
         
         if page=="Larvas":
             opcioneslarvas=st.selectbox("Selecciona:",("Control de Calidad","Parámetros","Alimento"),index=None)
@@ -28,46 +47,26 @@ if st.sidebar.checkbox("OK"):
                 st.write("Si observamos que el >95% de las larvas nadan activamente, entonces son puntuadas con un 10; si están activas del 70-95%, se puntúa 5; y si son <70% las activas entonces se puntúa 0 (FAO, 2004).")
                 actnat=st.text_input("Actividad natatoria: ")
                 
-                if st.button("ADD"):
-                    ct = datetime.datetime.now()
-                    connection = sqlite3.connect('cclarvas.db',detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
-                    cursor = connection.cursor()
-                    insertQuery = """INSERT INTO cclarvas VALUES (?, ?,?, ?, ?);"""
-                    cursor.execute(insertQuery, (ct,dia,tanque,estadio,actnat))
-                    connection.commit()
-                    st.success("Datos ingresados")
-        if page=="Buscar":
-            connection = sqlite3.connect('cclarvas.db',detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
-            cursor = connection.cursor()
-            cursor.execute("SELECT * from cclarvas")
-            fetchedData = cursor.fetchall()
-            df=pd.DataFrame(fetchedData,columns = ["fecha y hora","dia","tanque","estadio","act"])
+
+        
+        if page=="Salir":
+            authenticator.logout()
+       
+
+elif st.session_state["authentication_status"] is False:
+    st.error('Usuário/Senha is inválido')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Por Favor, utilize seu usuário e senha!')
+
+
+                
             
-            col1, col2, col3 = st.columns(3)
-            with col1:
-                st.write("Busca por tanque")
-                tanque=st.text_input("Ingresa tanque: ")
-                if st.button("Buscar",key="1"):
-                    st.dataframe(df[df['tanque'].str.contains(tanque)])
-            with col2:
-                st.write("Busca por dia")
-                dia=st.text_input("Ingresa dia: ")
-                if st.button("Buscar",key="2"):
-                    st.dataframe(df[df['dia'].str.contains(dia)])
-            with col3:
-                st.write("Busca por estadio")
-                estadio=st.text_input("Ingresa estadio: ")
-                if st.button("Buscar",key="3"):
-                    st.dataframe(df[df['estadio'].str.contains(estadio)])
+                   
              
             
 
    
-    else:
-        st.write("Usuario o contraseña incorrectos")               
- 
-            
-            
+    
             
             
             
